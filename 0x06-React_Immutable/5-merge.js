893dd613df05cb068c3813dfde945ba55d30a790
mooreArrqs <amandanxumalo921@gmail.com>
2024-09-13 21:15:47 +0000

    Merge & concat

--- 0x06-React_Immutable/5-merge.js
+++ 0x06-React_Immutable/5-merge.js
@@ -5,6 +5,12 @@ export function concatElements(page1, page2) {
   return List(page1).concat(List(page2));
 }
 
+// An example:
+
+const arr1 = [1, 2, 3];
+const arr2 = [4, 5, 6];
+console.log(concatElements(arr1, arr2));
+
 // This function merge two objects and return their values as an Immutable List
 export function mergeElements(page1, page2) {
   const mergedMap = Map(page1).merge(Map(page2)); // Convert and merge Immutable oblects
