662a52dbd32a16109f03b960e0b02830829417a1
mooreArrqs <amandanxumalo921@gmail.com>
2024-09-13 00:04:56 +0000

    Converting into Immutable using Map

---
+++ 0x06-React_Immutable/1-map.js
@@ -0,0 +1,31 @@
+// A function that converts an object into an Immutable Map
+
+// Import the Map method from Immutable.js
+const { Map } = require('immutable');
+
+
+/**
+ * Converts a JavaScript object into an immutable Map.
+ * @param {Object} object: The JavaScript object to convert.
+ * @returns {Map}: An immutable Map representation of the object.
+ */
+
+function getImmutableObject(object) {
+    return Map(object);
+}
+
+// An example:
+
+const obj = {
+    fear: true,
+    smell: -1033575916.9145899,
+    wall: false,
+    thing: -914767132
+};
+
+// Log the Immutable Map result
+console.log(getImmutableObject(obj));
+
+// Export the function
+module.exports = getImmutableObject;
+
